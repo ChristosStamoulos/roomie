304dc15033f4c17e19a9e31aab2400ee894562d4
Maria Schoinaki <shinaki54@gmail.com>
2024-03-27 18:33:49 +0200

    Updated Master.java

--- app/backend/src/Master.java
+++ app/backend/src/Master.java
@@ -2,9 +2,8 @@
 import java.net.*;
 import java.io.*;
 import java.util.*;
-
 //----------------------------------------------------------------Master-Class---------------------------------------------------------------
-public class Master extends Thread{
+public class Master{
     public static int num_of_workers;
     private static ObjectInputStream in; // Socket for Master to listen the requests from the Master
     private static ObjectOutputStream out; // Socket for each request to send the result to the Reducer
@@ -12,70 +11,36 @@ public class Master extends Thread{
     private static int reducer_port; // Port to send the result
     private static int user_port;
     private Chunk chunk;
-    public Socket requestSocket;
     ServerSocket workerSocket;
-    ServerSocket clientSocket;
     ServerSocket reducerSocket;
-//-------------------------------------------------------------Master-Constructor------------------------------------------------------------
-    public Master(Socket socket, Chunk chunk ){
-        this.chunk = chunk;
-        this.requestSocket = socket;
-    }
-//---------------------------------------------------------------Initialization--------------------------------------------------------------
-    public static void init(){
-        Properties prop = new Properties();
-        String filename = "app\\backend\\config\\master.config";
+    ServerSocket clientSocket;
 
-        try (FileInputStream f = new FileInputStream(filename)){
-            prop.load(f);
-        }catch (IOException exception ) {
-            System.err.println("I/O Error\n" + "The system cannot find the path specified");
-        }
-
-        Master.worker_port = Integer.parseInt(prop.getProperty("worker_port"));
-        Master.reducer_port = Integer.parseInt(prop.getProperty("reducer_port"));
-        Master.num_of_workers = Integer.parseInt(prop.getProperty("numberOfWorkers"));
-        System.out.println(Integer.parseInt(prop.getProperty("worker_port")));
-    }
-
-
-    @Override
-	public void run(){
-        try{
-            Master.out.writeObject(chunk);
-        }catch(IOException e){
-            System.err.println("I/O Error\n" + "Error");
-        }
-        System.out.println("hey i'm working!!");
-    }
+    ServerSocket providerSocket;
+    Socket socket = null;
 
     public static void main(String[] args){
-        Master.init();
-        ServerSocket connectionSocket;
-        Socket socket ;
-        //System.out.println(Master.host);
+        new Master().openMaster();
+    }
 
-        try{
-            connectionSocket = new ServerSocket(Master.worker_port);
-            socket = connectionSocket.accept();
-            System.out.println("Client accepteed");
-            Master.out = new ObjectOutputStream(socket.getOutputStream());
-            Master.out.writeUTF("Hello from Masrer!");
-            out.flush();
-            socket.close();
-            out.close();
-        }catch(IOException e){
-            System.err.println("I/O Error\n" + "Error occured while trying to create the socket for connection enstablishment");
-        }
+    void openMaster(){
+        try {
+			providerSocket = new ServerSocket(52153);
 
+			while (true) {
+				socket = providerSocket.accept();
 
-       /*  try{
-            Chunk chunk = new Chunk();
-            Socket chunkSocket = new Socket(Master.host, Master.reducer_port);
-            Master worker = new Master(chunkSocket, chunk);
-            worker.start();
-        }catch(IOException e){
-            System.err.println("I/O Error\n" + "Error occured while trying to create the socket for the request");
-        } */
-    }  
+				//Thread t = new ActionsForClients(connection);
+				//t.start();
+
+			}
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		} finally {
+			try {
+				providerSocket.close();
+			} catch (IOException ioException) {
+				ioException.printStackTrace();
+			}
+		}
+	}
 }
