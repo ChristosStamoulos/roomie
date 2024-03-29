f6e653832c9d2ab354a7810e6ce9639aabab5d11
Eleni Kehrioti <elenikehrioti@gmail.com>
2024-03-29 17:46:58 +0200

    added main functionality

--- app/backend/src/Chunk.java
+++ app/backend/src/Chunk.java
@@ -1,5 +1,24 @@
-public class Chunk {    
-     
+import java.io.Serializable;
 
+public class Chunk implements Serializable {    
+    private final String userID;
+    private final int segmentID;
 
+    public Chunk(String userID, int segmentID){//, Data data){
+        this.userID = userID;
+        this.segmentID = segmentID;
+        //this.data = data;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public int getSegmentID() {
+        return segmentID;
+    }
+
+    // public Data getData(){
+    //     return data;
+    // }
 }
