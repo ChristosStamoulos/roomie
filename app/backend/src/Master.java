1ece805de92912fbbd859cd56c60c36a4e31ef2a
Christos Stamoulos <stamcrist2@outlook.com>
2024-03-29 18:31:14 +0200

    Update Master.java

--- app/backend/src/Master.java
+++ app/backend/src/Master.java
@@ -9,12 +9,13 @@ public class Master extends Thread{
          ObjectInputStream  inWorker=null;
 
          try{
-            workerSocket = new Socket("192.168.54.71",1234);
+            workerSocket = new Socket("10.26.47.198",1234);
             outWorker = new ObjectOutputStream(workerSocket.getOutputStream());
 			inWorker = new ObjectInputStream(workerSocket.getInputStream());
 
-            outWorker.writeUTF("HIIII");
-            outWorker.flush();
+           
+
+            System.out.println(inWorker.readUTF());
 
          } catch (UnknownHostException unknownHost) {
 			System.err.println("You are trying to connect to an unknown host!");
