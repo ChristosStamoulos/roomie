14f57096584879a7794b2977b90c7ff274cd601b
Christos Stamoulos <stamcrist2@outlook.com>
2024-03-29 16:58:53 +0200

    updates

--- app/backend/src/Master.java
+++ app/backend/src/Master.java
@@ -1,62 +1,37 @@
-//------------------------------------------------------------------Imports------------------------------------------------------------------
-import java.net.*;
 import java.io.*;
-import java.util.*;
-//----------------------------------------------------------------Master-Class---------------------------------------------------------------
-public class Master{
-    public static int num_of_workers;
-    
-    private static int worker_port; // Port to listen the reques
-    private static int reducer_port; // Port to send the result
-    private static int user_port;
-    private Chunk chunk;
-    ServerSocket workerSocket;
-    ServerSocket reducerSocket;
-    ServerSocket clientSocket;
-    private static String host; //
-    private static ObjectInputStream in; // Socket for Master to listen the requests from the Master
-    private static ObjectOutputStream out; // Socket for each request to send the result to the Reducer
-    private static int listenPort; // Port to listen the request
-    private static int requestReducerPort; // Port to send the result
-    public Socket requestSocket;
+import java.net.*;
 
-    ServerSocket providerSocket;
-    Socket socket = null;
+public class Master extends Thread{
 
-    public static void init(){
-        Properties prop = new Properties();
-        String filename = "app\\backend\\config\\master.config";
+    public void run(){
+         Socket workerSocket=null;
+         ObjectOutputStream outWorker=null;
+         ObjectInputStream  inWorker=null;
 
-        try (FileInputStream f = new FileInputStream(filename)){
-            prop.load(f);
-        }catch (IOException exception ) {
-            System.err.println("I/O Error\n" + "The system cannot find the path specified");
-        }
+         try{
+            workerSocket = new Socket("192.168.54.71",1234);
+            outWorker = new ObjectOutputStream(workerSocket.getOutputStream());
+			inWorker = new ObjectInputStream(workerSocket.getInputStream());
 
-        Master.host = prop.getProperty("host");
-        Master.listenPort = Integer.parseInt(prop.getProperty("worker_port"));
-        Master.requestReducerPort = Integer.parseInt(prop.getProperty("reducer_port"));
-        Master.num_of_workers = Integer.parseInt(prop.getProperty("numberOfWorkers"));
-        System.out.println(Integer.parseInt(prop.getProperty("worker_port")));
+            outWorker.writeUTF("HIIII");
+            outWorker.flush();
+
+         } catch (UnknownHostException unknownHost) {
+			System.err.println("You are trying to connect to an unknown host!");
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		} finally {
+			try {
+				inWorker.close();	outWorker.close();
+				workerSocket.close();
+			} catch (IOException ioException) {
+				ioException.printStackTrace();
+			}
+		}
+
     }
 
-    public static void main(String[] args){
-        Master.init();
-        ServerSocket connectionSocket;
-        Socket socket ;
-        System.out.println(Master.host);
-
-        try{
-            connectionSocket = new ServerSocket(Master.worker_port);
-            socket = connectionSocket.accept();
-            System.out.println("Client accepteed");
-            Master.out = new ObjectOutputStream(socket.getOutputStream());
-            Master.out.writeUTF("Hello from Masrer!");
-            out.flush();
-            socket.close();
-            out.close();
-        }catch(IOException e){
-            System.err.println("I/O Error\n" + "Error occured while trying to create the socket for connection enstablishment");
-        }
+    public static void main(String[] args) {
+        new Master().start();
     }
 }
--- app/backend/src/Worker.java
+++ app/backend/src/Worker.java
@@ -1,84 +1,34 @@
-
-
-//------------------------------------------------------------------Imports------------------------------------------------------------------
-import java.net.*;
 import java.io.*;
-import java.util.*;
+import java.net.*;
 
-//----------------------------------------------------------------Worker-Class---------------------------------------------------------------
-public class Worker extends Thread{
-    private final int id;
-    private static String host; //
-    private static ObjectInputStream in; // Socket for Worker to listen the requests from the Master
-    private static ObjectOutputStream out; // Socket for each request to send the result to the Reducer
-    private static int listenPort; // Port to listen the request
-    private static int requestReducerPort; // Port to send the result
-    private Chunk chunk;
-    public Socket requestSocket;
-//-------------------------------------------------------------Worker-Constructor------------------------------------------------------------
-    public Worker(int id, Socket socket, Chunk chunk){
-        this.id = id;
-        this.chunk = chunk;
-        this.requestSocket = socket;
-    }
-//---------------------------------------------------------------Initialization--------------------------------------------------------------
-    public static void init(){
-        Properties prop = new Properties();
-        String filename = "app\\backend\\config\\worker.config";
-
-        try (FileInputStream f = new FileInputStream(filename)){
-            prop.load(f);
-        }catch (IOException exception ) {
-            System.err.println("I/O Error\n" + "The system cannot find the path specified");
-        }
-
-        Worker.host = prop.getProperty("host");
-        Worker.listenPort = Integer.parseInt(prop.getProperty("listenPort"));
-        Worker.requestReducerPort = Integer.parseInt(prop.getProperty("requestReducerPort"));
-        System.out.println(Integer.parseInt(prop.getProperty("listenPort")));
-    }
-
-    @Override
-	public void run(){
-        try{
-            Worker.out.writeObject(chunk);
-        }catch(IOException e){
-            System.err.println("IO Error\n" + "Error");
-        }
-        System.out.println("hey i'm working!!");
-    }
+public class Worker {
 
     public static void main(String[] args) {
-        Worker.init();
-        Socket connectionSocket; 
-        System.out.println(Worker.host);
+        new Worker().openServer();
 
-        try{
-            connectionSocket = new Socket(Worker.host, Worker.listenPort);
-            Worker.in = new ObjectInputStream(connectionSocket.getInputStream());
-            System.out.println("Found Master!");
-            while(in.available() > 0){
-                String message = in.readUTF();
-            System.out.println("Server says: " + message);
-            }
-            connectionSocket.close();
-            in.close();;
-
-        }catch(IOException e){
-            System.err.println("I/O Error\n" + "Error occured while trying to create the socket for conection enstablishment");
-        }
-
-
-        try{
-            Chunk chunk = (Chunk) Worker.in.readObject();
-            Socket chunkSocket = new Socket(Worker.host, Worker.requestReducerPort);
-            Worker worker = new Worker(1, chunkSocket, chunk);
-            worker.start();
-        }catch(ClassNotFoundException e){
-            System.err.println("Unexpected error: " + e);
-        }catch(IOException e){
-            System.err.println("I/O Error\n" + "Error occured while trying to create the socket for the request");
-        }
     }
-}
+    ServerSocket providerSocket;
+	Socket connection = null;
 
+    void openServer() {
+		try {
+			providerSocket = new ServerSocket(1234, 10);
+
+			while (true) {
+				connection = providerSocket.accept();
+
+				//Thread t = new ActionsForClients(connection);
+				//t.start();
+
+			}
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		} finally {
+			try {
+				providerSocket.close();
+			} catch (IOException ioException) {
+				ioException.printStackTrace();
+			}
+		}
+	}
+}
