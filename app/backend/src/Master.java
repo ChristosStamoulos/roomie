b1ad6a5f96e9d06d283f98868861713cec8af221
Christos Stamoulos <stamcrist2@outlook.com>
2024-03-30 11:21:59 +0200

    upadate config

--- app/backend/src/Master.java
+++ app/backend/src/Master.java
@@ -1,7 +1,30 @@
 import java.io.*;
 import java.net.*;
+import java.util.Properties;
 
 public class Master extends Thread{
+    public static int num_of_workers;
+    private static String host;
+    private static int workerPort;
+    private static int ReducerPort;
+
+     public static void init(){
+        Properties prop = new Properties();
+        String filename = "app\\backend\\config\\master.config";
+
+        try (FileInputStream f = new FileInputStream(filename)){
+            prop.load(f);
+        }catch (IOException exception ) {
+            System.err.println("I/O Error\n" + "The system cannot find the path specified");
+        }
+
+        Master.host = prop.getProperty("host");
+        Master.workerPort = Integer.parseInt(prop.getProperty("worker_port"));
+        Master.ReducerPort = Integer.parseInt(prop.getProperty("reducer_port"));
+        Master.num_of_workers = Integer.parseInt(prop.getProperty("numberOfWorkers"));
+        System.out.println(Integer.parseInt(prop.getProperty("worker_port")));
+    }
+
 
     public void run(){
          Socket workerSocket=null;
@@ -9,7 +32,7 @@ public class Master extends Thread{
          ObjectInputStream  inWorker=null;
 
          try{
-            workerSocket = new Socket("10.26.47.198",1234);
+            workerSocket = new Socket("192.168.1.4",1234);
             outWorker = new ObjectOutputStream(workerSocket.getOutputStream());
 			inWorker = new ObjectInputStream(workerSocket.getInputStream());
 
--- app/backend/src/Worker.java
+++ app/backend/src/Worker.java
@@ -1,7 +1,29 @@
 import java.io.*;
 import java.net.*;
+import java.util.Properties;
 
 public class Worker {
+	
+    private static String host;
+    private static int masterPort;
+    private static int ReducerPort;
+	private static int serverPort;
+
+	public static void init(){
+        Properties prop = new Properties();
+        String filename = "app\\backend\\config\\worker.config";
+
+        try (FileInputStream f = new FileInputStream(filename)){
+            prop.load(f);
+        }catch (IOException exception ) {
+            System.err.println("I/O Error\n" + "The system cannot find the path specified");
+        }
+
+        Worker.host = prop.getProperty("host");
+        Worker.masterPort = Integer.parseInt(prop.getProperty("masterPort"));
+		Worker.serverPort = Integer.parseInt(prop.getProperty("serverPort"));
+        System.out.println(Integer.parseInt(prop.getProperty("serverPort")));
+    }
 
     public static void main(String[] args) {
         new Worker().openServer();
@@ -12,7 +34,7 @@ public class Worker {
 
     void openServer() {
 		try {
-			providerSocket = new ServerSocket(1234, 10);
+			providerSocket = new ServerSocket(Worker.serverPort, 10);
 
 			while (true) {
 				connection = providerSocket.accept();
