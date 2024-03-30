1753f1a77a015994da5652c809c38b033058fb74
Eleni Kehrioti <elenikehrioti@gmail.com>
2024-03-30 13:51:43 +0200

    dummy user start

---
+++ app/backend/DummyUser/DummyUser.java
@@ -0,0 +1,14 @@
+public class DummyUser {
+    
+    private static void printMenu(){
+        System.out.println(
+        """
+            1. Add room
+            2. Add more available dates for your rooms.
+            3. See the reservations of your rooms.
+                """);
+    }
+    public static void main(String[] args) {
+        printMenu();
+    }
+}
