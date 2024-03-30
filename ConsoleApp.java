06af9d9812eb6cb2d48041b5652d75cb8c97ebe6
Eleni Kehrioti <elenikehrioti@gmail.com>
2024-03-30 14:19:47 +0200

    basic functionality

--- ConsoleApp.java
+++ ConsoleApp.java
@@ -1,5 +1,6 @@
+import java.util.*;
+
 public class ConsoleApp {
-    
 
     private static void printMenu(){
         System.out.println(
@@ -7,9 +8,30 @@ public class ConsoleApp {
             1. Add room
             2. Add more available dates for your rooms.
             3. See the reservations of your rooms.
+            4. Exit
                 """);
     }
     public static void main(String[] args) {
-        printMenu();
+        Scanner in = new Scanner(System.in);
+        while(true){
+            printMenu();
+            System.out.print("Your choice: ");
+            int choice = Integer.parseInt(in.nextLine());
+            
+            switch (choice){
+                case 1:
+                    System.out.println("Give the relative path to the file with the room data");
+                    String roomPath = in.nextLine();
+                    break;
+                case 2:
+                break;
+                case 3:
+                break;
+                case 4:
+                    System.exit(0);
+            }
+        }
+        
+        
     }
 }
