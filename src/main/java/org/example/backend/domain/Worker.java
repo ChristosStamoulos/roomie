d25df89b96a5785e0b51163257dd453271e1fe18
Maria Schoinaki <shinaki54@gmail.com>
2024-04-16 18:33:49 +0300

    Updated Worker

--- src/main/java/org/example/backend/domain/Worker.java
+++ src/main/java/org/example/backend/domain/Worker.java
@@ -24,154 +24,43 @@ import java.util.Properties;
  */
 
 public class Worker {
-    private final int id;
-    private static int masterPort;
+    private static int id = 0;
     private static int reducerPort;
     private static int serverPort;
-    private static ArrayList<Room> rooms;
-    private static ObjectInputStream in;
-    private static ObjectOutputStream out;
-    static ServerSocket providerSocket;
-    static Socket masterConnection = null;
-    private static Socket reducerSocket = null;
     private static String reducerHost;
-    private static JsonConverter jsonConverter;
 
-    Worker(int id){
-        this.id = id;
-        this.rooms = new ArrayList<Room>();
-    }
-
-    public static void init(){
+    public static void init() {
         Properties prop = new Properties();
         String filename = "src/main/java/org/example/backend/config/worker.config";
 
-
-        try (FileInputStream f = new FileInputStream(filename)){
+        try (FileInputStream f = new FileInputStream(filename)) {
             prop.load(f);
-        }catch (IOException exception ) {
-            System.err.println("I/O Error\n" + "The system cannot find the path specified");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
         }
 
-        Worker.serverPort = Integer.parseInt(prop.getProperty("serverPort"));
-        Worker.masterPort = Integer.parseInt(prop.getProperty("masterPort"));
-        Worker.reducerPort = Integer.parseInt(prop.getProperty("reducerPort"));
-        Worker.reducerHost = prop.getProperty("reducerHost");
-        System.out.println(Integer.parseInt(prop.getProperty("serverPort")));
-        jsonConverter = new JsonConverter();
-    }
-
-
-    /**
-     * Adds room to the workers "memory"
-     * @param room a Room object
-     */
-    private void addRoom(Room room){
-        if(!rooms.contains(room)){
-            rooms.add(room);
-        }
+        serverPort = Integer.parseInt(prop.getProperty("serverPort"));
+        reducerPort = Integer.parseInt(prop.getProperty("reducerPort"));
+        reducerHost = prop.getProperty("reducerHost");
     }
 
     public static void main(String[] args) {
         init();
-        //openServer();
-
-
-            try {
-                providerSocket = new ServerSocket(Worker.serverPort);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            //reducerSocket = new Socket(Worker.reducerHost, Worker.reducerPort);
-
-
-            Thread master = new Thread(() -> {
-                while (!Thread.currentThread().isInterrupted()) {
-                    try {
-                        masterConnection = providerSocket.accept();
-                        in = new ObjectInputStream(masterConnection.getInputStream());
-                        System.out.println("Master connected");
-                        try {
-                            System.out.println(in.readObject().toString());
-                            Chunk data = (Chunk) in.readObject();
-                            System.out.println(data.getData().toString());
-                            reducerSocket = new Socket(Worker.reducerHost, Worker.reducerPort);
-                            if (data.getTypeID() == 1) {
-                                rooms.add(jsonConverter.convertToRoom(new JSONObject(data.getData())));
-                            } else {
-                                Thread workerThread = new ActionsForWorkers(data, rooms, reducerSocket);//,reducerSocket);
-                                workerThread.start();
-                            }
-
-                        } catch (ClassNotFoundException e) {
-                            e.printStackTrace();
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-            master.start();
-
-//            Thread reducer = new Thread(() -> {
-//                while (!Thread.currentThread().isInterrupted()) {
-//
-//                    try {
-//                        reducerSocket = new Socket(Worker.reducerHost, Worker.reducerPort);
-//                        ObjectOutputStream ou = new ObjectOutputStream(reducerSocket.getOutputStream());
-//                        ou.writeObject(new Chunk("i", 3, new ArrayList<>()));
-//                        ou.flush();
-//
-//                    } catch (UnknownHostException e) {
-//                        e.printStackTrace();
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            });
-//            reducer.start();
-
+        openServer();
     }
 
-    static void openServer() {
-        try {
-            providerSocket = new ServerSocket(Worker.serverPort);
-            //reducerSocket = new Socket(Worker.reducerHost, Worker.reducerPort);
+    private static void openServer() {
+        try (ServerSocket providerSocket = new ServerSocket(serverPort, 100);
+             Socket reducerSocket = new Socket(reducerHost, reducerPort)) {
+
             while (true) {
-                masterConnection = providerSocket.accept();
-                System.out.println("hhhhhhhh");
-                in = new ObjectInputStream(masterConnection.getInputStream());
-
-                try {
-                    while(!Thread.currentThread().isInterrupted()) {
-
-                        System.out.println(in.readObject().toString());
-                        Chunk data = (Chunk) in.readObject();
-                        System.out.println(data.getData().toString());
-                        if(data.getTypeID() == 1){
-                            rooms.add(jsonConverter.convertToRoom(new JSONObject( data.getData())));
-                        }else {
-                            //Thread workerThread = new ActionsForWorkers(data, rooms);//,reducerSocket);
-                            //workerThread.start();
-                        }
-                        //System.out.println("Worker #" + id + " assigned data: " + data);
-                    }
-                } catch (ClassNotFoundException e) {
-                    throw new RuntimeException(e);
-                }
-                //System.out.println("oups here");
-//                Thread t = new ActionsForClients(masterConnection);
-//                t.start();
-
+                Socket masterConnection = providerSocket.accept();
+                System.out.println("Master connected");
+                new ActionsForWorkers(masterConnection, reducerSocket).start();
             }
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        } finally {
-            try {
-                providerSocket.close();
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
