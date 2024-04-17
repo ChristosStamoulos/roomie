24edafed3627d6515d432a48be6e1cc590c3cc5d
Maria Schoinaki <shinaki54@gmail.com>
2024-04-17 23:19:02 +0300

    Update Reducer.java

--- src/main/java/org/example/backend/domain/Reducer.java
+++ src/main/java/org/example/backend/domain/Reducer.java
@@ -1,34 +1,38 @@
 package org.example.backend.domain;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+
 /** Reducer Class
  *
  * @author Maria Schoinaki, Eleni Kechrioti, Christos Stamoulos
  * @Details This project is being carried out in the course Distributed Systems @ Spring AUEB 2024.
  *
- * This class is implemented to handle the room results from  Workers reducing them and sending them back to Master groped by their unique segment ID.
+ * This class is implemented to handle the room results from Workers reducing them and sending them back to Master groped by their unique segment ID.
  */
 public class Reducer {
-    private static int serverPort;
-    private static String masterHost;
-    private static int masterPort;
-    private static int expectedChunks;
+    private static int serverPort;       //The port number on which the server listens for incoming connections from Workers.
+    private static String masterHost;    //The hostname of the Master server.
+    private static int masterPort;       //The port number of the Master server.
+    private static int expectedChunks;   //The number of expected chunks to be received before processing.
+    //A map to store chunks received from Workers based on their segment IDs.
     private static Map<Integer, ArrayList<Chunk>> chunkMap = new HashMap<>();
-
+    //The number of chunks to be processed concurrently.
     private static final int numberOfChunks = 1;
+
     /**
      * Initialises the variables from the config file
      */
     public static void init() {
+        // Load configuration from file
         Properties prop = new Properties();
         String filename = "src/main/java/org/example/backend/config/reducer.config";
 
@@ -39,42 +43,53 @@ public class Reducer {
             return;
         }
 
+        // Read properties from the configuration file
         masterHost = prop.getProperty("masterHost");
         masterPort = Integer.parseInt(prop.getProperty("masterPort"));
         serverPort = Integer.parseInt(prop.getProperty("serverPort"));
         expectedChunks = Integer.parseInt(prop.getProperty("expectedChunks"));
     }
 
+    /**
+     * Main class of Reducer
+     * @param args Default parameters
+     */
     public static void main(String[] args) {
+        // Initialize the system components
         init();
+        // Start the reducer server
         startReducerServer();
     }
+
     /**
-     * Opens a Reducer server to listen for requests from workers,
-     * and handles those requests in handleWorkerRequest method.
+     * Starts the Reducer server, which listens for incoming Worker connections.
      */
     private static void startReducerServer() {
         try (ServerSocket providerSocket = new ServerSocket(serverPort, 100)) {
+            // Continuously accept incoming worker connections
             while (true) {
                 Socket workerSocket = providerSocket.accept();
                 System.out.println("Worker connected");
 
+                // Handle each worker request in a separate thread
                 new Thread(() -> handleWorkerRequest(workerSocket)).start();
             }
         } catch (IOException ex) {
+            // If an exception occurs, wrap it in a RuntimeException and throw it
             throw new RuntimeException(ex);
         }
     }
+
     /**
-     * Handles a request made by a worker.
-     * If the amount of a specific type of request has reached its expected
-     * limits, then the reduction process begins
+     * Handles a Worker request by reading chunks sent by the Worker.
+     *
+     * @param workerSocket The socket connected to the Worker.
      */
     private static void handleWorkerRequest(Socket workerSocket) {
         try {
             ObjectInputStream in = new ObjectInputStream(workerSocket.getInputStream());
             while (true) {
-                Chunk chunk = (Chunk) in.readObject(); // Read chunks sent by the worker
+                Chunk chunk = (Chunk) in.readObject(); // Read chunks sent by the Worker
                 // Add the chunk to the map based on its ID
                 synchronized (chunkMap) {
                     int chunkId = chunk.getSegmentID();
@@ -83,23 +98,28 @@ public class Reducer {
                     }
                     chunkMap.get(chunkId).add(chunk);
 
+                    // Check if all expected chunks are received
                     if (chunkMap.get(chunkId).size() == expectedChunks) {
                         ArrayList<Chunk> chunks = chunkMap.get(chunkId);
+                        // Merge chunks into a single chunk
                         Chunk mergedChunk = mergeChunks(chunks);
+                        // Send merged chunk to the Master
                         sentToMaster(mergedChunk);
-
+                        // Clear the chunks from the map
                         chunkMap.remove(chunkId); // Clear the chunks from the map
                     }
                 }
             }
         } catch (IOException | ClassNotFoundException e) {
+            // Print stack trace if an exception occurs
             e.printStackTrace();
         }
     }
+
     /**
-     * The reducer acts as a client to master,
-     * and now it's time to send back the reduced
-     * form of data that he has received
+     * Sends the given chunk to the Master.
+     *
+     * @param chunk The chunk to be sent to the Master.
      */
     private static void sentToMaster(Chunk chunk) {
         try {
@@ -109,31 +129,37 @@ public class Reducer {
             out.writeObject(chunk); // Send the merged chunk to the master
             out.flush();
 
-            // Close connections
-            out.close();                                    ///////////////////////////////////////////////////////////////////////
-            masterSocket.close();                           ///////////////////////////////////////////////////////////////////////
+            // Close connection
+            out.close();
+            masterSocket.close();
         } catch (IOException e) {
+            // Print stack trace if an exception occurs
             e.printStackTrace();
         }
     }
+
     /**
-     Helper method for handleWorkerRequest.
-     Returns a final chunk that contains a list of merged data
-     This list contains all the data gathered by a group of specific requests
+     * Merges the list of chunks into a single chunk.
+     *
+     * @param chunks The list of chunks to be merged.
+     * @return The merged chunk containing the combined list of rooms.
      */
     private static Chunk mergeChunks(ArrayList<Chunk> chunks) {
+        // Extract userID, segmentID, and type from the first chunk
         String userID = chunks.get(0).getUserID();
         int id = chunks.get(0).getSegmentID();
         int type = chunks.get(0).getSegmentID();
 
         ArrayList<Room> workerChunk = new ArrayList<>();
         ArrayList<Room> finalList = new ArrayList<>();
+        // Iterate over each chunk to extract the list of rooms and add them to the final list
         for (int i = 0; i < expectedChunks; i++) {
             workerChunk = (ArrayList<Room>) chunks.get(i).getData();
             for (int j = 0; j < workerChunk.size(); j++) {
                 finalList.add(workerChunk.get(j));
             }
         }
+        // Create a new chunk with the merged list of rooms and set the userID, segmentID, and type
         Chunk finalChunk = new Chunk(userID, type, finalList);
         finalChunk.setSegmentID(id);
         return finalChunk;
