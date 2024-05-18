c528aba7b857bbc5f9f4fa03b1bf1168ca319a9d
Maria Schoinaki <shinaki54@gmail.com>
2024-05-19 02:06:00 +0300

    updated manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,21 +24,28 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
         jvmTarget = "17"
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
 dependencies {
 
+    implementation("com.google.android.material:material:1.5.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation("androidx.core:core-ktx:+")
+    implementation("androidx.core:core-ktx")
+    implementation("com.prolificinteractive:material-calendarview:1.4.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
