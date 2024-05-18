8a9a8802cd42dd09e80d4d9ab2c5c6afc121df3d
Maria Schoinaki <shinaki54@gmail.com>
2024-05-19 02:07:58 +0300

    created class for calendar

---
+++ app/src/main/java/com/example/chambre/frontend/AvailabilityDecorator.kt
@@ -0,0 +1,17 @@
+package com.example.chambre.frontend
+
+import android.graphics.Color
+import android.text.style.ForegroundColorSpan
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
+
+class AvailabilityDecorator(private val dates: HashSet<CalendarDay>) : DayViewDecorator {
+    override fun shouldDecorate(day: CalendarDay): Boolean {
+        return dates.contains(day)
+    }
+
+    override fun decorate(view: DayViewFacade) {
+        view.addSpan(ForegroundColorSpan(Color.GREEN)) // Available dates highlighted in green
+    }
+}
