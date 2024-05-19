c59957c13689c4e2cf95134afe3cc275419b37ff
Eleni Kehrioti <elenikehrioti@gmail.com>
2024-05-19 18:39:20 +0300

    created an adapter for rooms display

--- frontend/app/src/main/java/com/example/roomie/frontend/Adapters/RoomsAdapter.kt
+++ frontend/app/src/main/java/com/example/roomie/frontend/Adapters/RoomsAdapter.kt
@@ -11,9 +11,9 @@ import com.example.roomie.R
 import com.example.roomie.backend.domain.Room
 import java.util.Locale
 
-class RoomsAdapter (private val roomList: ArrayList<Room>, private val onRoomClickListener: OnRoomClickListener) : RecyclerView.Adapter<RoomsAdapter.RoomViewHolder>() {
-    interface OnRoomClickListener {
-        fun onRoomClick(room: Room?)
+class RoomsAdapter (private val roomList: ArrayList<Room>, private val listener: onRoomClickListener) : RecyclerView.Adapter<RoomsAdapter.RoomViewHolder>() {
+    interface onRoomClickListener {
+        fun onRoomClick(room: Room, view: View)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RoomViewHolder {
@@ -23,7 +23,7 @@ class RoomsAdapter (private val roomList: ArrayList<Room>, private val onRoomCli
 
     override fun onBindViewHolder(holder: RoomViewHolder, position: Int) {
         val room = roomList[position]
-        holder.bindRoom(room)
+        holder.bindRoom(room, listener)
     }
 
     override fun getItemCount(): Int {
@@ -40,29 +40,39 @@ class RoomsAdapter (private val roomList: ArrayList<Room>, private val onRoomCli
             roomName = itemView.findViewById(R.id.name)
             roomArea = itemView.findViewById(R.id.roomArea)
             roomStars = itemView.findViewById(R.id.roomStars)
-            itemView.setOnClickListener {
-                val adapterPosition = getAdapterPosition()
-                if (adapterPosition != RecyclerView.NO_POSITION) {
-
-                    if (adapterPosition < roomList.size) {
-                        val clickedRoom = roomList[adapterPosition]
-                        onRoomClickListener.onRoomClick(clickedRoom)
-                    }
-                }
-            }
         }
 
-        fun bindRoom(room: Room) {
+        fun bindRoom(room: Room, listener: onRoomClickListener) {
             roomName = itemView.findViewById(R.id.name)
             roomImage = itemView.findViewById(R.id.roomImage)
             roomName.text = room.name
             roomArea.text = room.area
             roomStars.text = room.rating.toString()
-            val resId = itemView.resources.getIdentifier(room.name.lowercase(Locale.getDefault()), "drawable", itemView.context.packageName)
+            /*val resId = itemView.resources.getIdentifier(room.name.lowercase(Locale.getDefault()), "drawable", itemView.context.packageName)
 
             // Set the image based on the resource identifier
             if (resId != 0) {
                 roomImage!!.setImageResource(resId)
+            }*/
+
+            itemView.setOnClickListener {
+                // Create the click animation
+                it.animate()
+                        .scaleX(0.9f) // Scale down the X axis to 90%
+                        .scaleY(0.9f) // Scale down the Y axis to 90%
+                        .setDuration(100) // Duration of the scale down animation
+                        .withEndAction {
+                            // Scale back to original size
+                            it.animate()
+                                    .scaleX(1.0f)
+                                    .scaleY(1.0f)
+                                    .setDuration(100)
+                                    .withEndAction {
+                                        listener.onRoomClick(room, it)
+                                    }
+                                    .start()
+                        }
+                        .start()
             }
         }
     }
