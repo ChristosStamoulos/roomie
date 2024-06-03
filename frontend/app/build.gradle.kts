600ea366c9b6135c6e833f5b75ddcfad14f60487
Eleni Kehrioti <elenikehrioti@gmail.com>
2024-06-03 23:27:45 +0300

    Update build.gradle.kts

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -37,14 +37,14 @@ android {
 
 dependencies {
 
-    implementation("com.google.android.material:material:1.5.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.android.gms:play-services-maps:18.0.2")
     implementation("com.google.android.flexbox:flexbox:3.0.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation("androidx.core:core-ktx")
+    implementation("androidx.core:core-ktx:1.6.0")
     implementation("com.prolificinteractive:material-calendarview:1.4.3")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.1")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.1")
