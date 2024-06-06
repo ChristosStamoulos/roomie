a01c78317cf957169ef44fb3dfb5a19c9dc45030
Maria Schoinaki <shinaki54@gmail.com>
2024-06-06 19:43:09 +0300

    added start screen

---
+++ frontend/app/src/main/java/com/example/roomie/frontend/StartSreenActivity.kt
@@ -0,0 +1,51 @@
+package com.example.roomie.frontend
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.roomie.R
+
+class StartSreenActivity : AppCompatActivity() {
+    private lateinit var signUpButton: Button
+    private lateinit var logInButton: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.enableEdgeToEdge()
+        setContentView(R.layout.activity_start_screen)
+
+        signUpButton = findViewById(R.id.btnSignUp)
+        logInButton = findViewById(R.id.btnLogIn)
+        
+        signUpButton.setOnClickListener {
+            signUp()
+        }
+
+        logInButton.setOnClickListener {
+            logIn()
+        }
+    }
+
+    private fun signUp(){
+        val intent = Intent(this@StartSreenActivity, SignUpActivity::class.java)
+        val options = ActivityOptionsCompat.makeCustomAnimation(
+                this@StartSreenActivity,
+                R.anim.expand_trans,  // Animation for the entering activity
+                R.anim.slide_out      // Animation for the exiting activity
+        )
+    }
+
+    private fun logIn(){
+        val intent = Intent(this@StartSreenActivity, LogInActivity::class.java)
+        val options = ActivityOptionsCompat.makeCustomAnimation(
+                this@StartSreenActivity,
+                R.anim.expand_trans,  // Animation for the entering activity
+                R.anim.slide_out      // Animation for the exiting activity
+        )
+    }
+}
